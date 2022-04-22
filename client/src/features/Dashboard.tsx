c1d1e310d54ea31f0ba040b2ef5ec981922add8d
LukaFlores <luka.flores@mail.com>
2022-04-22 09:40:38 -0700

    feature(client): add example google maps

--- client/src/features/Dashboard.tsx
+++ client/src/features/Dashboard.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
 
 const Dashboard: React.FC<{ loading: boolean }> = (props) => {
-  return <div className="flex justify-center mt-20 bg-white">This is the dashbaord</div>;
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
+  };
+
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        //bootstrapURLKeys={{ key: '' }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 export default Dashboard;
